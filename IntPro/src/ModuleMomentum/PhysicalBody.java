f90e592b28f84a732eb72d91e6421f72e1c23b33
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-03-18 14:26:57 -0400

    Add files via upload

---
+++ IntPro/src/ModuleMomentum/PhysicalBody.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuleMomentum;
+import intpro.Element;
+/**
+ *
+ * @author Amine
+ */
+public class PhysicalBody extends Element{
+    double mass;
+    double momentumX;
+    double momentumY;
+    
+    public PhysicalBody(double mass, double velX, double velY) {
+        super(velX,velY);
+        this.mass = mass;
+        this.momentumX = mass * velX;
+        this.momentumY = mass * velY;
+        
+    }
+    public double getMass() {
+        return mass;
+    }
+    public double getMomentumX() {
+        return momentumX;
+    }
+    public double getMomentumY() {
+        return momentumY;
+    }
+    
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+    public void setMomentumX(double momentumX) {
+        this.momentumX = momentumX;
+    }
+    public void setMomentumY(double momentumY) {
+        this.momentumY = momentumY;
+    }
+}
