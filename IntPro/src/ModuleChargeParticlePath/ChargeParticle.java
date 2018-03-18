5336bc8efb4c41f197f69e31e79b5fc48749ec33
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-03-18 17:58:58 -0400

    Update ChargeParticle.java

--- IntPro/src/ModuleChargeParticlePath/ChargeParticle.java
+++ IntPro/src/ModuleChargeParticlePath/ChargeParticle.java
@@ -14,9 +14,27 @@ public class ChargeParticle extends Element{
     double electricForce = 0;
     
     public ChargeParticle(double charge) {
+        super(1,1,0,0);
         this.charge = charge;
     }
     public ChargeParticle(double charge,double velX, double velY) {
+        super(velX,velY,0,0);
         this.charge = charge;
+    }
+    public ChargeParticle(double charge,double velX, double velY, double posX, double posY) {
+        super(velX, velY, posX, posY);
+        this.charge = charge;
+    }
+    public double getCharge(){
+        return charge;
+    }
+    public double getElectricForce() {
+        return electricForce;
+    }
+    public void setCharge(double charge){
+        this.charge = charge;
+    }
+    public void setElectricForce(double electricForce) {
+        this.electricForce = electricForce;
     }
 }
