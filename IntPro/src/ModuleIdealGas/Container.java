e037242965eebc3e54835c453a138c508ff9e9b9
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-04-23 18:31:08 -0400

    Add files via upload

---
+++ IntPro/src/ModuleIdealGas/Container.java
@@ -0,0 +1,15 @@
+
+package ModuleIdealGas;
+
+/**
+ *
+ * @author 1630954
+ */
+import intpro.Element;
+import java.util.ArrayList;
+import javafx.scene.shape.Rectangle;
+
+public class Container extends Element{
+    ArrayList<GasParticle> particles = new ArrayList<>();
+    
+}
---
+++ IntPro/src/ModuleIdealGas/GasParticle.java
@@ -0,0 +1,12 @@
+
+package ModuleIdealGas;
+
+/**
+ *
+ * @author 1630954
+ */
+import intpro.SpritedElement;
+
+public class GasParticle extends SpritedElement{
+    
+}
