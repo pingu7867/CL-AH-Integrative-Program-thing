8635ff750fdec78e3e369da1727e8607fd9a6874
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-04-27 13:16:28 -0400

    Add files via upload

--- IntPro/src/ModuleIdealGas/Container.java
+++ IntPro/src/ModuleIdealGas/Container.java
@@ -9,6 +9,7 @@ import intpro.SpritedElement;
 import java.util.ArrayList;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
+import javafx.scene.shape.Rectangle;
 
 public class Container extends SpritedElement{
     ArrayList<GasParticle> particles = new ArrayList<>();
@@ -16,9 +17,16 @@ public class Container extends SpritedElement{
     double volume = 0; //in liters (L)
     double pressure = 0; //in kPa
     double temperature = 0; //in Kelvin (K)
+    
+    boolean unlockAOM = false;
+    boolean unlockVol = false;
+    boolean unlockPre = false;
+    boolean unlockTemp = false;
+    
     double averageKineticEnergy = 0;
     final double GAS_CONSTANT = 8.3144598;
     final double BOLTZMANN_CONSTANT = 1.38064852 * Math.pow(10, -23); //in eV/K
+    Rectangle hitbox;
     
     public Container() {
         super(new ImageView());
@@ -43,16 +51,61 @@ public class Container extends SpritedElement{
     }
     
     public void setMoles(double aom) {
+        double ratioVN = volume/amountOfMole;
+        double ratioPN = pressure/amountOfMole;
+                
         amountOfMole = aom;
+        
+        if (!unlockPre) {
+            volume = ratioVN * aom;
+        }
+        else if (!unlockVol) {
+            pressure = ratioPN * aom;
+        }
     }
     public void setVolume(double vol) {
+        double kVP = volume * pressure;
+        double ratioVT = volume/temperature;
+        double ratioVN = volume/amountOfMole;
+        
         volume = vol;
+        if (!unlockAOM && !unlockTemp) {
+            pressure = kVP/vol;
+        }
+        else if (!unlockAOM && !unlockPre) {
+            temperature = vol/ratioVT;
+        }
+        else if (!unlockTemp && !unlockPre) {
+            amountOfMole = vol/ratioVN;
+        }
+        
     }
     public void setPressure(double pre) {
+        double kVP = volume * pressure;
+        double ratioPN = pressure/amountOfMole;
+        double ratioPT = pressure/temperature;
+        
         pressure = pre;
+        
+        if (!unlockVol && !unlockAOM) {
+            temperature = pre/ratioPT;
+        }
+        else if (!unlockVol && !unlockTemp) {
+            amountOfMole = pre/ratioPN;
+        }
+        else if (!unlockAOM && !unlockTemp) {
+            volume = kVP/pre;
+        }
     }
     public void setTemperature(double temp) {
+        double ratioVT = volume/temperature;
+        double ratioPT = pressure/temperature;
+        
         temperature = temp;
+        
+        if (!unlockVol) {
+            
+        }
     }
     
 }
--- IntPro/src/ModuleIdealGas/GasParticle.java
+++ IntPro/src/ModuleIdealGas/GasParticle.java
@@ -1,12 +1,93 @@
 
 package ModuleIdealGas;
-
 /**
  *
  * @author 1630954
  */
 import intpro.SpritedElement;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.animation.*;
+import javafx.util.Duration;
+import java.io.File;
+import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
+import javafx.scene.shape.Arc;
 
 public class GasParticle extends SpritedElement{
+    private Timeline animation;
+    ArrayList<Arc> arcs = new ArrayList<>();
+    private double velocity;
+    
+    public GasParticle() {
+        super(new ImageView());
+        this.angle = Math.random() * (2*Math.PI);
+        velocity = 0;
+        sprite.setImage(new Image(new File("src/Assets/Molecule0.png").toURI().toString()));
+    }
+    public GasParticle(double averageKineticEnergy) {
+        this();
+        velocity = (averageKineticEnergy * 2)/1.6737236 * Math.pow(10, -27);
+        velX = velocity * Math.cos(angle);
+        velY = velocity * Math.sin(angle);
+        posX = 0; posY = 0;
+        sprite.setX(posX); sprite.setY(posY);
+        sprite.setFitHeight(10); sprite.setFitWidth(10);
+    }
+    public double getVelocity() {
+        return velocity;
+    }
+    public void setHitBox() {
+        double startingAngle = 0;
+        while (startingAngle < 360) {
+            arcs.add(new Arc(sprite.getX() + sprite.getFitWidth(), sprite.getY() + sprite.getFitHeight(), sprite.getFitWidth()/2, sprite.getFitHeight()/2
+            ,startingAngle,1));
+            startingAngle+=5;
+        }
+    }
+    public void animateGasParticleMotion(ArrayList<GasParticle> gasParticles,int index,Rectangle bounds) {
+        boolean quit1 = false;
+        boolean quit2 = false;
+        for (int i = index + 1; i < gasParticles.size(); i++) {
+            if (this.sprite.intersects(gasParticles.get(i).sprite.getX(), gasParticles.get(i).sprite.getY(),
+               gasParticles.get(i).sprite.getFitWidth(), gasParticles.get(i).sprite.getFitHeight())) {
+               for (int j = 0; j < arcs.size(); j++) {
+                   for (int k = 0; k < gasParticles.get(i).arcs.size(); k++) {
+                       if (arcs.get(j).intersects(gasParticles.get(i).arcs.get(k).getBoundsInLocal())) {
+                           
+                           gasParticles.get(i).setAngle((gasParticles.get(i).getAngleRad() + arcs.get(j).getStartAngle()) % (2*Math.PI));
+                           gasParticles.get(i).setVelX(gasParticles.get(i).getVelocity() * Math.cos(gasParticles.get(i).getAngleRad()));
+                           gasParticles.get(i).setVelY(gasParticles.get(i).getVelocity() * Math.sin(gasParticles.get(i).getAngleRad()));
+                           
+                           angle = (angle + gasParticles.get(i).arcs.get(k).getStartAngle()) % (2*Math.PI);
+                           velX = velocity * Math.cos(angle);
+                           velY = velocity * Math.sin(angle);
+                           quit1 = true; quit2 = true;
+                           break;
+                           
+                       }
+                   }
+                   if (quit1) {
+                       break;
+                   }
+               }
+               if (quit2) {
+                   break;
+               }
+           }
+           
+        }
+        if (posX + 5 > bounds.getX() + bounds.getWidth() || posX < 5 + bounds.getX()) {
+               velX *= -1;
+           }
+           if (posY + 5 > bounds.getY() + bounds.getHeight() || posY < 5 + bounds.getY()) {
+               velY *= -1;
+           }
+        posX += velX/60; posY -= velY/60;
+        sprite.setX(sprite.getX() + velX/60); sprite.setY(sprite.getY() - velY/60);
+        for (int i = 0; i < arcs.size(); i++) {
+            
+        }
+    }
     
 }
