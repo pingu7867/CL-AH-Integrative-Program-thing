917f3c0247ea2a2b3f02770b76541c115faaefc7
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-04-30 13:52:26 -0400

    Update ProjectileMotionModule.java

--- IntPro/src/ModuleProjectileMotion/ProjectileMotionModule.java
+++ IntPro/src/ModuleProjectileMotion/ProjectileMotionModule.java
@@ -56,4 +56,65 @@ public class ProjectileMotionModule extends Module {
     @Override
     public void tickProcess() {
     }
+    
+    public generateCannonWindow() {
+     Stage stage = new Stage();
+        VBox WindowLayout = new VBox(20);
+        CheckBox checkToPutMountedCannon= new CheckBox();
+        
+        TextField fieldforVelX = new TextField("1");
+        fieldforVelX .setPrefColumnCount(3);
+        fieldforVelX .textProperty().addListener(ov-> {
+            String text = fieldforVelX.getText();
+            if (!checkDecimal(text) && text.contains("-")) {
+               text = text.substring(0, text.length() - 1);
+               text.replace("-", "");
+               fieldforVelX .setText(text);
+            }
+        });
+        
+        TextField fieldforVelY = new TextField("0");
+        fieldforVelY.setPrefColumnCount(3);
+        fieldforVelY.textProperty().addListener(ov-> {
+            String text = fieldforVelY.getText();
+            if (!checkDecimal(text) && text.contains("-")) {
+               text.replace("-", "");
+               text = text.substring(0, text.length() - 1);
+               fieldforVelY.setText(text);
+            }
+        });
+        
+        Label labelforVelX = new Label("Separation Distance of the 2 plates (m)", fieldforVelX);
+        labelforVelX.setContentDisplay(ContentDisplay.RIGHT);
+        
+        Label labelforVelY = new Label("Orientation (degrees)", fieldforVelY);
+        labelforVelY.setContentDisplay(ContentDisplay.RIGHT);
+        
+        HBox buttons = new HBox(8);
+        buttons.setAlignment(Pos.BOTTOM_RIGHT);
+        Button Create = new Button("Create");        
+        Button Cancel = new Button("Cancel");
+        buttons.getChildren().addAll(Cancel, Create);
+        
+        Cancel.setOnAction(eh-> {
+            stage.close();
+        });
+        Create.setOnAction(eh -> {
+           
+           double velXValue = Double.parseDouble(fieldforVelX.getText());
+           double velYValue = Double.parseDouble(fieldforVelY.getText());
+           Cannon cannon = new Cannon(velXValue,velYValue);
+           body.setBody();
+           listOfPhysicalBodies.add(body);
+           stage.close();
+        });
+        
+        WindowLayout.getChildren().addAll(labelforHeight, labelforRadiusLeft, labelforRadiusRight, buttons);
+        if (!viewport.isShowing()) {
+            stage.close();
+        }
+        stage.setScene(new Scene(WindowLayout));
+        stage.requestFocus();
+        return stage;
+    }   
 }
