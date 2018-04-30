69ce19151e698400b5b609b44433d4bd70b78502
kilner99 <35849317+kilner99@users.noreply.github.com>
2018-04-30 13:10:29 -0400

    Update SystemOfLens.java

--- IntPro/src/ModuleLensOptics/SystemOfLens.java
+++ IntPro/src/ModuleLensOptics/SystemOfLens.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package ModuleLensOptics;
-
+import intpro.*
+import java.util.ArrayList;
 /**
  *
  * @author Amine
  */
-public class SystemOfLens {
+public class SystemOfLens extends Element{
+    ArrayList<Lens> listOfLens = new ArrayList<>();
+    double vergence = 0;
+    double focalLength;
     
+    public SystemOfLens() {
+        
+    }
+    public void addLen(Lens len) {
+        listOfLens.add(len);
+    }
+    public void addLens(Lens[] lens) {
+        listOfLens.addAll(lens);
+    }
+    public void addLens(List<Lens> lens) {
+        listOfLens.addAll(lens);
+    }
+    public void setVergenceAdded() {
+        for (int i = 0; i < listOfLens.size(); i++) {
+           vergence += listOfLens.get(i).getVergence();   
+        }
+        focalLength = 1/vergence;
+    }
+    public double getVergence() {
+        return vergence;
+    }
+    public double getFocalLength() {
+        return focalLength;   
+    }
 }
